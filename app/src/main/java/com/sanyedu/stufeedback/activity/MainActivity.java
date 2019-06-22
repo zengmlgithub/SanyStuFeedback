ee1a30bdb709bcc6878d1491706e5009060aafc3
zengmaolin <zengml@sany.com.cn>
2019-06-22 12:26:49 +0800

    增加个人中心的跳转

--- app/src/main/java/com/sanyedu/stufeedback/activity/MainActivity.java
+++ app/src/main/java/com/sanyedu/stufeedback/activity/MainActivity.java
@@ -3,13 +3,18 @@ package com.sanyedu.stufeedback.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.sanyedu.sanylib.base.SanyBaseActivity;
 import com.sanyedu.sanylib.log.SanyLogs;
+import com.sanyedu.sanylib.share.SpHelper;
 import com.sanyedu.sanylib.utils.StartUtils;
 import com.sanyedu.stufeedback.R;
+import com.sanyedu.stufeedback.model.StudentModel;
 import com.sanyedu.stufeedback.mvp.MainInfo.MainContacts;
 import com.sanyedu.stufeedback.mvp.MainInfo.MainPresenter;
+import com.sanyedu.stufeedback.utils.StuContantsUtil;
+import com.sanyedu.stufeedback.utils.UserInfoHelper;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +24,7 @@ public class MainActivity extends SanyBaseActivity<MainPresenter> implements Mai
 
     @BindView(R.id.operator_rl)
     RelativeLayout operatorLayout;
+
 
     @OnClick({R.id.all_feedback_iv,R.id.notice_iv,R.id.my_feedback_iv,R.id.feedback_my_iv,R.id.add_civ,R.id.head_rl})
     public void myOnclick(View view){
@@ -43,19 +49,26 @@ public class MainActivity extends SanyBaseActivity<MainPresenter> implements Mai
         }
     }
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ButterKnife.bind(this);
-        ButterKnife.bind(R.layout.layout_main_operator,operatorLayout);
+
     }
 
     @Override
     protected void initData() {
+        ButterKnife.bind(this);
+        ButterKnife.bind(R.layout.layout_main_operator,operatorLayout);
+        initHead();
+    }
 
+    private void initHead() {
+        TextView headTv = findViewById(R.id.head_tv);
+        StudentModel studentModel = SpHelper.getObj(StuContantsUtil.STUINFO);
+        if(studentModel != null) {
+            String name = studentModel.getStuName();
+            headTv.setText(name);
+        }
     }
 
     @Override
